776222989928410fc889fdaec17e3351a4c75c0c
jennymatoka12-bit <jennymatoka12@gmail.com>
2026-09-17 18:48:06 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -40,123 +40,68 @@ class HomeScreen extends StatelessWidget {
             indicatorColor: Color(0xFFE50914),
             indicatorWeight: 3,
             tabs: [
-              Tab(icon: Icon(Icons.tv), text: 'Chaînes TV'),
-              Tab(icon: Icon(Icons.radio), text: 'Radios'),
+              Tab(icon: Icon(Icons.tv), text: 'TV Direct'),
+              Tab(icon: Icon(Icons.radio), text: 'Radio Direct'),
             ],
           ),
         ),
-        body: TabBarView(
+        body: const TabBarView(
           children: [
-            _buildGrid(context, isTv: true),
-            _buildGrid(context, isTv: false),
+            TvRadioZapViewer(url: 'https://tvradiozap.eu/live/x/vlc/d/tvzeu.m3u'),
+            TvRadioZapViewer(url: 'https://tvradiozap.eu/'),
           ],
         ),
       ),
     );
   }
-
-  Widget _buildGrid(BuildContext context, {required bool isTv}) {
-    final List<Map<String, String>> items = isTv
-        ? [
-            {'title': 'Gabon 24', 'sub': 'Information Direct', 'url': 'https://www.youtube.com/embed/live_stream?channel=UC4S4M3m_xN0wO1xX10sV7pA'},
-            {'title': 'Africanews FR', 'sub': 'Info Afrique 24/7', 'url': 'https://www.youtube.com/embed/live_stream?channel=UC3O31k3q5FqjS3J59PzOqDA'},
-            {'title': 'France 24', 'sub': 'Direct International', 'url': 'https://www.youtube.com/embed/live_stream?channel=UC24e03xM893f1X-2a1M94gA'},
-            {'title': 'TV5Monde', 'sub': 'Généraliste', 'url': 'https://www.youtube.com/embed/live_stream?channel=UCq_yF9mHq-sL710F5A_8rQg'},
-          ]
-        : [
-            {'title': 'RFI Afrique', 'sub': 'Actualités & Culture', 'url': 'https://www.rfi.fr/fr/podcasts/'},
-            {'title': 'Africa N°1 / Radio', 'sub': 'Musique & Info', 'url': 'https://www.youtube.com/embed/live_stream?channel=UC3O31k3q5FqjS3J59PzOqDA'},
-            {'title': 'Urban FM', 'sub': 'Hits & Jeunesse', 'url': 'https://www.youtube.com'},
-            {'title': 'Gabon Culture', 'sub': 'Patrimoine', 'url': 'https://www.youtube.com'},
-          ];
-
-    return GridView.builder(
-      padding: const EdgeInsets.all(16),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 12,
-        mainAxisSpacing: 12,
-        childAspectRatio: 1.1,
-      ),
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        final item = items[index];
-        return Card(
-          color: const Color(0xFF1F1F1F),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-          child: InkWell(
-            borderRadius: BorderRadius.circular(16),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PlayerScreen(title: item['title']!, url: item['url']!),
-                ),
-              );
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 26,
-                    backgroundColor: const Color(0xFFE50914).withOpacity(0.2),
-                    child: Icon(
-                      isTv ? Icons.play_arrow_rounded : Icons.radio_outlined,
-                      size: 32,
-                      color: const Color(0xFFE50914),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    item['title']!,
-                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Text(
-                    item['sub']!,
-                    style: const TextStyle(fontSize: 11, color: Colors.grey),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
 }
 
-class PlayerScreen extends StatefulWidget {
-  final String title;
+class TvRadioZapViewer extends StatefulWidget {
   final String url;
-
-  const PlayerScreen({super.key, required this.title, required this.url});
+  const TvRadioZapViewer({super.key, required this.url});
 
   @override
-  State<PlayerScreen> createState() => _PlayerScreenState();
+  State<TvRadioZapViewer> createState() => _TvRadioZapViewerState();
 }
 
-class _PlayerScreenState extends State<PlayerScreen> {
+class _TvRadioZapViewerState extends State<TvRadioZapViewer> {
   late final WebViewController _controller;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            setState(() => _isLoading = true);
+          },
+          onPageFinished: (String url) {
+            setState(() => _isLoading = false);
+            // Injection CSS pour masquer l'entête, le tableau d'affichage et garder uniquement la zone vidéo
+            _controller.runJavaScript('''
+              var style = document.createElement('style');
+              style.innerHTML = 'header, footer, nav, .top-bar, #header, #footer { display: none !important; } body { background-color: #121212 !important; color: white !important; }';
+              document.head.appendChild(style);
+            ''');
+          },
+        ),
+      )
       ..loadRequest(Uri.parse(widget.url));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: WebViewWidget(controller: _controller),
+    return Stack(
+      children: [
+        WebViewWidget(controller: _controller),
+        if (_isLoading)
+          const Center(
+            child: CircularProgressIndicator(color: Color(0xFFE50914)),
+          ),
+      ],
     );
   }
 }

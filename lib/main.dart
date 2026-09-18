deea0ffde6a349f553d76bde241a1d2236581143
jennymatoka12-bit <jennymatoka12@gmail.com>
2026-09-18 10:44:28 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
-import 'package:chewie/chewie.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const NgombiApp());
@@ -27,61 +26,66 @@ class NgombiApp extends StatelessWidget {
   }
 }
 
-class Channel {
+class MediaItem {
   final String name;
-  final String streamUrl;
+  final String url;
   final String category;
   final bool isRadio;
 
-  Channel({
+  MediaItem({
     required this.name,
-    required this.streamUrl,
+    required this.url,
     required this.category,
     this.isRadio = false,
   });
 }
 
-// Flux vidéo et audio directs HLS / M3U8 (100% stables sans navigateur)
-final List<Channel> tvChannels = [
-  Channel(
-    name: 'France 24 Français Direct',
-    streamUrl: 'https://static.france24.com/live/F24_FR_LO_HLS/live_web-audio=100000-video=1500000.m3u8',
-    category: 'Information',
+// Liste des chaînes pointant vers le portail TVRadioZap et le flux YouTube pour le Gabon
+final List<MediaItem> tvChannels = [
+  MediaItem(
+    name: 'Gabon Télévision',
+    url: 'https://www.youtube.com/embed/live_stream?channel=UC8g9p1S3pT4j--81z1c5Xkg',
+    category: 'Gabon - Direct YouTube',
   ),
-  Channel(
-    name: 'Euronews Français',
-    streamUrl: 'https://euronews-euronews-french-1-fr.samsung.wurl.tv/manifest/playlist.m3u8',
-    category: 'Information',
+  MediaItem(
+    name: 'TVRadioZap (Portail TV)',
+    url: 'https://tvradiozap.eu/',
+    category: 'Chaînes Françaises & Int.',
   ),
-  Channel(
-    name: 'TV5Monde Info',
-    streamUrl: 'https://ott.tv5monde.com/Content/HLS/Live/channel(info)/index.m3u8',
-    category: 'Information',
+  MediaItem(
+    name: 'TF1 (via TVRadioZap)',
+    url: 'https://tvradiozap.eu/',
+    category: 'Généraliste',
   ),
-  Channel(
-    name: 'Africanews Français',
-    streamUrl: 'https://africanews-africanews-french-1-fr.samsung.wurl.tv/manifest/playlist.m3u8',
-    category: 'Afrique',
+  MediaItem(
+    name: 'France 2 (via TVRadioZap)',
+    url: 'https://tvradiozap.eu/',
+    category: 'Généraliste',
+  ),
+  MediaItem(
+    name: 'M6 (via TVRadioZap)',
+    url: 'https://tvradiozap.eu/',
+    category: 'Généraliste',
   ),
 ];
 
-final List<Channel> radioChannels = [
-  Channel(
+final List<MediaItem> radioChannels = [
+  MediaItem(
+    name: 'TVRadioZap (Portail Radios)',
+    url: 'https://tvradiozap.eu/',
+    category: 'Radios en Direct',
+    isRadio: true,
+  ),
+  MediaItem(
     name: 'RFI Afrique',
-    streamUrl: 'https://live02.rfi.fr/rfiafrique-64.mp3',
+    url: 'https://www.rfi.fr/fr/en-direct',
     category: 'Information',
     isRadio: true,
   ),
-  Channel(
+  MediaItem(
     name: 'Africa Radio',
-    streamUrl: 'https://africaradio.ice.infomaniak.ch/africaradio-128.mp3',
+    url: 'https://www.africaradio.com/',
     category: 'Musique & Infos',
-    isRadio: true,
-  ),
-  Channel(
-    name: 'RFI Monde',
-    streamUrl: 'https://live02.rfi.fr/rfimonde-64.mp3',
-    category: 'Information',
     isRadio: true,
   ),
 ];
@@ -95,7 +99,7 @@ class MainTabScreen extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('NGOMBI Direct', style: TextStyle(fontWeight: FontWeight.bold)),
+          title: const Text('NGOMBI TV & Radio', style: TextStyle(fontWeight: FontWeight.bold)),
           bottom: const TabBar(
             indicatorColor: Color(0xFFE50914),
             indicatorWeight: 3,
@@ -107,8 +111,8 @@ class MainTabScreen extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            ChannelList(channels: tvChannels),
-            ChannelList(channels: radioChannels),
+            MediaListView(items: tvChannels),
+            MediaListView(items: radioChannels),
           ],
         ),
       ),
@@ -116,34 +120,34 @@ class MainTabScreen extends StatelessWidget {
   }
 }
 
-class ChannelList extends StatelessWidget {
-  final List<Channel> channels;
-  const ChannelList({super.key, required this.channels});
+class MediaListView extends StatelessWidget {
+  final List<MediaItem> items;
+  const MediaListView({super.key, required this.items});
 
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
       padding: const EdgeInsets.all(12),
-      itemCount: channels.length,
+      itemCount: items.length,
       separatorBuilder: (context, index) => const Divider(color: Colors.white10),
       itemBuilder: (context, index) {
-        final channel = channels[index];
+        final item = items[index];
         return ListTile(
           leading: CircleAvatar(
             backgroundColor: const Color(0xFFE50914).withOpacity(0.2),
             child: Icon(
-              channel.isRadio ? Icons.radio : Icons.play_arrow_rounded,
+              item.isRadio ? Icons.radio : Icons.play_arrow_rounded,
               color: const Color(0xFFE50914),
             ),
           ),
-          title: Text(channel.name, style: const TextStyle(fontWeight: FontWeight.bold)),
-          subtitle: Text(channel.category, style: const TextStyle(color: Colors.grey, fontSize: 12)),
-          trailing: const Icon(Icons.play_circle_fill, color: Color(0xFFE50914), size: 28),
+          title: Text(item.name, style: const TextStyle(fontWeight: FontWeight.bold)),
+          subtitle: Text(item.category, style: const TextStyle(color: Colors.grey, fontSize: 12)),
+          trailing: const Icon(Icons.arrow_forward_ios, color: Colors.grey, size: 16),
           onTap: () {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => NativePlayerScreen(channel: channel),
+                builder: (context) => WebPlayerScreen(title: item.name, url: item.url),
               ),
             );
           },
@@ -153,103 +157,84 @@ class ChannelList extends StatelessWidget {
   }
 }
 
-class NativePlayerScreen extends StatefulWidget {
-  final Channel channel;
+class WebPlayerScreen extends StatefulWidget {
+  final String title;
+  final String url;
 
-  const NativePlayerScreen({super.key, required this.channel});
+  const WebPlayerScreen({super.key, required this.title, required this.url});
 
   @override
-  State<NativePlayerScreen> createState() => _NativePlayerScreenState();
+  State<WebPlayerScreen> createState() => _WebPlayerScreenState();
 }
 
-class _NativePlayerScreenState extends State<NativePlayerScreen> {
-  late VideoPlayerController _videoPlayerController;
-  ChewieController? _chewieController;
-  bool _hasError = false;
+class _WebPlayerScreenState extends State<WebPlayerScreen> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _initializePlayer();
-  }
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setUserAgent(
+        "Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36",
+      )
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            setState(() {
+              _isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            // Nettoyage de l'interface du site web d'origine
+            _controller.runJavaScript('''
+              try {
+                document.querySelector('header')?.style.setProperty('display', 'none', 'important');
+                document.querySelector('footer')?.style.setProperty('display', 'none', 'important');
+                document.querySelector('.ads')?.style.setProperty('display', 'none', 'important');
+              } catch(e) {}
+            ''');
 
-  Future<void> _initializePlayer() async {
-    try {
-      _videoPlayerController = VideoPlayerController.networkUrl(
-        Uri.parse(widget.channel.streamUrl),
-      );
-
-      await _videoPlayerController.initialize();
-
-      _chewieController = ChewieController(
-        videoPlayerController: _videoPlayerController,
-        autoPlay: true,
-        looping: true,
-        isLive: true,
-        allowFullScreen: true,
-        aspectRatio: widget.channel.isRadio ? 16 / 9 : _videoPlayerController.value.aspectRatio,
-        errorBuilder: (context, errorMessage) {
-          return Center(
-            child: Text(
-              'Erreur de lecture du flux.\n$errorMessage',
-              textAlign: TextAlign.center,
-              style: const TextStyle(color: Colors.white),
-            ),
-          );
-        },
-      );
-
-      setState(() {});
-    } catch (e) {
-      setState(() {
-        _hasError = true;
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    _videoPlayerController.dispose();
-    _chewieController?.dispose();
-    super.dispose();
+            if (mounted) {
+              setState(() {
+                _isLoading = false;
+              });
+            }
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.url));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.channel.name)),
-      body: Center(
-        child: _hasError
-            ? const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Impossible de charger ce flux en direct.\nVérifiez votre connexion internet.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.redAccent, fontSize: 16),
-                ),
-              )
-            : _chewieController != null && _chewieController!.videoPlayerController.value.isInitialized
-                ? widget.channel.isRadio
-                    ? Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(Icons.radio, size: 100, color: Color(0xFFE50914)),
-                          const SizedBox(height: 20),
-                          Text(
-                            widget.channel.name,
-                            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 20),
-                          const CircularProgressIndicator(color: Color(0xFFE50914)),
-                          const SizedBox(height: 10),
-                          const Text('Diffusion radio en cours...'),
-                        ],
-                      )
-                    : AspectRatio(
-                        aspectRatio: _videoPlayerController.value.aspectRatio,
-                        child: Chewie(controller: _chewieController!),
-                      )
-                : const CircularProgressIndicator(color: Color(0xFFE50914)),
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () async {
+              if (await _controller.canGoBack()) {
+                await _controller.goBack();
+              }
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () => _controller.reload(),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _controller),
+          if (_isLoading)
+            const Center(
+              child: CircularProgressIndicator(color: Color(0xFFE50914)),
+            ),
+        ],
       ),
     );
   }
